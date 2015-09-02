fe76da8a01dd6917770212aee5b62f0f305133d3
Leonardo Sampaio Ferraz Ribeiro <leo.sampaio.ferraz.ribeiro@gmail.com>
2015-09-02 16:04:36 -0300

    FIX: conserta indices no script

--- threadtest.sh
+++ threadtest.sh
@@ -1,24 +1,21 @@
 #! /bin/bash
 
+test_file=matriz1500.txt
 
 for((j=1;j<10;j+=1))
 do
 
-	total_time_single=0
 	total_time_multi=0
 
 	for((i=0;i<2;i++))
 	do
-		(/usr/bin/time -f "\n%e" ./build/main < matrizes/$test_file) 2> tmp-$test_file 1> result-$test_file
-		total_time_single=$(python -c "print $total_time_single + $(tail -n 1 tmp-$test_file)")
 
-		(/usr/bin/time -f "\n%e" ./build/main --multi < matrizes/$test_file) 2> tmp-$test_file 1> result-$test_file
+		(/usr/bin/time -f "\n%e" ./build/main --multi --nthreads $j < matrizes/$test_file) 2> tmp-$test_file 1> result-$test_file
 		total_time_multi=$(python -c "print $total_time_multi + $(tail -n 1 tmp-$test_file)")
 	done
 
-	echo Threads: $i
+	echo Threads: $j
 	echo $test_file
-	echo Average Singlethreaded Time: $(python -c "print \"%.3f\" % ($total_time_single/10)")s
 	echo Average Multithreaded Time: $(python -c "print \"%.3f\" % ($total_time_multi/10)")s
 	echo 
 
